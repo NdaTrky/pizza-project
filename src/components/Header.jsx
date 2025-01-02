b92bb35f84dc693491c58f6ce00b6e9c75758278
nıdaturkay <145764089+NdaTrky@users.noreply.github.com>
2025-01-02 18:05:08 +0300

    success ana teması hazırlandı.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,23 @@
 import React from 'react';
 import { Button } from "@/components/ui/button"
 import {useNavigate} from "react-router-dom"
+import '../App.css';
 
 function Header() {
+  const navigate = useNavigate();
 
-const navigate = useNavigate();
-
-const handleOrderClick = () => {
-  navigate("/order");
-}
-
+  const handleOrderClick = () => {
+    navigate("/order");
+  }
 
   return (
     <div className="relative h-screen w-screen">
-      {/* Metinler */}
-      <div className="absolute inset-x-0 top-20 text-center text-white space-y-6 sm:space-y-8">
-        <h1 className="text-3xl sm:text-5xl font-bold font-roboto">Teknolojik Yemekler</h1>
-        <p className="text-lg sm:text-2xl text-sari font-satisfy">Fırsatı Kaçırma!</p>
-        <h2 className="text-3xl sm:text-6xl text-koyuGri font-roboto">KOD ACIKTIRIR, PİZZA DOYURUR.</h2>
+      <div className="absolute inset-x-0 top-20 text-center space-y-6 sm:space-y-8">
+        <h1 className="title">Teknolojik Yemekler</h1>
+        <p className="text">Fırsatı Kaçırma!</p>
+        <h2 className="subtitle">KOD ACIKTIRIR, PİZZA DOYURUR.</h2>
         <Button variant="default" onClick={handleOrderClick}>Acıktım</Button>
       </div>
-      
-      {/* Arka Plan Görseli */}
       <img 
         src="/home-banner.png" 
         alt="header.logo" 
--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 function Success() {
   return (
-    <div className="text-kirmizi">Success</div>
+    <div className="bg-kirmizi h-screen">
+      <div className='absolute inset-x-0 top-20 text-center space-y-6 sm:space-y-8'>
+      <h1 className='title'>Teknolojik Yemekler</h1>
+      <p className='text'>Lezzetin Yolda!</p>
+      <h2 className='subtitle'>Sipariş Başarıyla Alındı.</h2>
+      </div>
+    </div>
   )
 }
 
